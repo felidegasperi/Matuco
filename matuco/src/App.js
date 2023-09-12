861d9eb2f38b9722b4bedadc06994b7a018065f9
Alvaro Johansen <109972764+Alvarojohansen@users.noreply.github.com>
2023-09-12 14:17:36 -0300

    NavBar planteado

--- matuco/src/App.js
+++ matuco/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import NavBar from './components/navBar/NavBar';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>Matuco</h1>
+      <Home/>
     </div>
   );
 }
