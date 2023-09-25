dff8ba065ad1e9a78f03567563cf63c714bbf026
Felipe De Gasperi <felidegasperi@gmail.com>
2023-09-25 20:28:29 -0300

    added buttons and bug fixes

--- matuco/src/components/navBar/NavBar.js
+++ matuco/src/components/navBar/NavBar.js
@@ -4,13 +4,14 @@ const NavBar = () => {
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
+        <div class="container fw-bold fs-5">
           <a class="navbar-brand" href="#">
             <img
               style={{ width: "100px", height: "90px" }}
               src={"../assets/icon.png"}
             />
           </a>
+
           {/* boton para el dropdown */}
           {/* <button
             class="navbar-toggler"
@@ -23,35 +24,37 @@ const NavBar = () => {
           >
             <span class="navbar-toggler-icon"></span>
           </button> */}
-          <div
-            style={{
-              fontSize: "20px",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              fontSize: "20px",
-            }}
-            id="navbarSupportedContent"
-          >
-            <Row className="navbar-nav mb-2 mb-lg-auto text-bg-dark p-3">
-              <Col className="nav-item mx-2 py-2">
-                <a className="nav-link" href="#">
-                  Inicio
-                </a>
-              </Col>
-              <Col className="nav-item mx-2 py-2">
-                <a className="nav-link " href="#">
-                  Productos
-                </a>
-              </Col>
-              <Col className="nav-item dropdown mx-2 py-2">
-                <a className="nav-link dropdown-toggle">Carrito</a>      
-              </Col>
-              <Col className="d-inline-flex p-2  mx-4 py-1 text-light bg-dark">
-                <p className="">Hola -username-!</p>
-              </Col>
-            </Row>
-          </div>
+
+          <Row className="navbar-nav mb-2 mb-lg-auto text-bg-dark p-2">
+            <Col className="nav-item mx-2 py-2">
+              <a className="nav-link" href="#">
+                Inicio
+              </a>
+            </Col>
+            <Col className="nav-item  py-2">
+              <a className="nav-link " href="#">
+                Productos
+              </a>
+            </Col>
+            <Col className="py-2">
+              <a className="nav-link" href="#">
+                Carrito
+              </a>
+            </Col>
+            <Col className="mx-4 text-light bg-dark">
+              <p className="">Hola -username-!</p>
+            </Col>
+            <Col className="d-flex">
+              <button className="btn btn-primary btn-sm p-2 m-2 " type="button">
+                Iniciar Sesion
+              </button>
+            </Col>
+            <Col className="d-flex">
+              <button className="btn btn-primary btn-sm p-2 m-2" type="button">
+                Registrarse
+              </button>
+            </Col>
+          </Row>
         </div>
       </nav>
     </>
