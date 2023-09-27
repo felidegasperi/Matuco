45b23d79d8edc1b7f4013035f05ab55ef9dd3492
Alvaro Johansen <109972764+Alvarojohansen@users.noreply.github.com>
2023-09-27 00:20:24 -0300

    login ready to mershear

--- matuco/src/components/login/Login.js
+++ matuco/src/components/login/Login.js
@@ -7,13 +7,19 @@ import NavBar from "../navBar/NavBar";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
   const changeEmailHandler = (e) => {
+    if (emailRef.current.value.length > 0) {
+      emailRef.current.style.borderColor = "";
+      emailRef.current.style.outline = "";
+    }
     setEmail(e.target.value);
   };
+
   const changePasswordHandler = (e) => {
     setPassword(e.target.value);
   };
@@ -22,69 +28,91 @@ const Login = () => {
       emailRef.current.focus();
       emailRef.current.style.borderColor = "red";
       emailRef.current.style.outline = "none";
-      alert("Email vacío!");
+      setError("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+      
+
       return;
     }
 
-    if (password.length === 0) {
+    if (password.length === 0 ) {
       passwordRef.current.focus();
       passwordRef.current.style.borderColor = "red";
       passwordRef.current.style.outline = "none";
-      alert("Password vacío");
+      setError(
+        "Credenciales incorrectas. Por favor, inténta rellendando con una contraseña "
+      );
       return;
-    }
-    alert("Ingresaste!");
+    }else if(password.length <=8){
+      passwordRef.current.focus();
+      passwordRef.current.style.borderColor = "red";
+      passwordRef.current.style.outline = "none";
+      setError(
+        "Credenciales incorrectas. Por favor, inténtalo de nuevo con una contraseña más larga."
+      );
+      return;
+    };
+    alert(`Su email es: ${email} y su password es: ${password}`);
   };
   return (
-    <div>
-      <NavBar />
-      <div className="d-flex justify-content-end mb-3 w-auto container mt-5 p-5">
-        <div className="col-md-7">
-          <img
-            src="../" // Reemplaza con la ruta de tu imagen
-            alt="Imagen de inicio de sesión"
-            className="img-fluid" // Para hacer la imagen responsive
-          />
-        </div>
-        <div className="col-md-5">
-          <h2>Iniciar sesion</h2>
-          <form className="border rounded-3 p-5 ">
-            <div className="input-conteiner mt-3 mw-100 mb-4">
-              <input
-                className="form-control form-control-lg"
-                value={email}
-                onChange={changeEmailHandler}
-                placeholder="Ingrese su email"
-                type="email"
-                ref={emailRef}
+    <>
+      <div>
+        <NavBar />
+        <div className="container-fluid ml-0">
+          <div className="row ml-0">
+            <div className="col-md-6 d-flex align-items-center justify-content-center ml-0 ">
+              <img
+                src="../assets/mates-login.jpg" // Reemplaza con la ruta de tu imagen
+                alt="Imagen de inicio de sesión"
+                className="img-custom-size" 
               />
             </div>
-            <div className="input-conteiner mt-3 mb-4">
-              <input
-                ref={passwordRef}
-                value={password}
-                onChange={changePasswordHandler}
-                className="form-control form-control-lg"
-                placeholder="Ingrese su contraseña"
-                type="password"
-              />
+            <div className="col-md-6 d-flex align-items-center p-5 ">
+              <div className="container">
+                <form className="border rounded-3 p-5 ">
+                  <h2>Iniciar sesión</h2>
+                  <div className="input-conteiner mt-3 mw-100 mb-4">
+                    <input
+                      className="form-control form-control-lg"
+                      value={email}
+                      onChange={changeEmailHandler}
+                      placeholder="Ingrese su email"
+                      type="email"
+                      ref={emailRef}
+                    />
+                  </div>
+
+                  <div className="input-conteiner mt-3 mb-4">
+                    <input
+                      ref={passwordRef}
+                      value={password}
+                      onChange={changePasswordHandler}
+                      className="form-control form-control-lg"
+                      placeholder="Ingrese su contraseña"
+                      type="password"
+                    />
+                  </div>
+                  <div className="text-danger">{error}</div>
+                  <div>
+                    <a href="#" class="link-primary">
+                      Deseas registrarte?Clickea aqui
+                    </a>
+                  </div>
+                  <div className="vstack mt-3 align-self-center">
+                    <button
+                      onClick={loginHandler}
+                      type="button"
+                      className="btn btn-outline-secondary "
+                    >
+                      Iniciar Sesion
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
-            <div>
-              <a href="#">Deseas registrarte?Clickea aqui</a>
-            </div>
-            <div className="vstack mt-3 align-self-center">
-              <button
-                onClick={loginHandler}
-                type="button"
-                className="btn btn-outline-success "
-              >
-                Iniciar Sesion
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
