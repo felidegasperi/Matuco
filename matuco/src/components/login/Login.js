cca1194364a0c1aa09621b61e9097131e3a5326e
Felipe De Gasperi <felidegasperi@gmail.com>
2023-11-02 17:23:16 -0300

    added login logic

--- matuco/src/components/login/Login.js
+++ matuco/src/components/login/Login.js
@@ -1,125 +1,157 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { useContext } from "react";
+import { ThemeContext } from "../../services/themeContext/Theme.context";
 
 import "./Login.css";
 
 import NavBar from "../navBar/NavBar";
 import Footer from "../footer/Footer";
 
-import { useContext } from "react";
-import { ThemeContext } from "../../services/themeContext/Theme.context";
-
 const Login = () => {
+  const [users, setUsers] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
+  const navigate = useNavigate();
   const { theme } = useContext(ThemeContext);
 
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
+  useEffect(() => {
+    // GET a la API y almacenar los usuarios en el estado
+    fetch("http://localhost:8000/users", {
+      headers: {
+        accept: "aplication/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error al obtener los usuarios", error));
+  }, []);
 
-  const changeEmailHandler = (e) => {
-    if (emailRef.current.value.length > 0) {
-      emailRef.current.style.borderColor = "";
-      emailRef.current.style.outline = "";
+  //funcion que va a manejar el envio del formulario
+  const handleSumbit = (e) => {
+    e.preventDefault();
+
+    // esto verifica si los datos ingresados son iguales a los de algun usuario de la db
+    const user = users.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (user) {
+      alert("Usuario ingresado correctamente");
+      console.log(user)
+    } else {
+      alert("Error al iniciar sesion");
     }
-    setEmail(e.target.value);
   };
 
-  const changePasswordHandler = (e) => {
-    setPassword(e.target.value);
-  };
-  const onLoginHandler = () => {
-    if (emailRef.current.value.length === 0) {
-      emailRef.current.focus();
-      emailRef.current.style.borderColor = "red";
-      emailRef.current.style.outline = "none";
-      setError("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+  // const changeEmailHandler = (e) => {
+  //   if (emailRef.current.value.length > 0) {
+  //     emailRef.current.style.borderColor = "";
+  //     emailRef.current.style.outline = "";
+  //   }
+  //   setEmail(e.target.value);
+  // };
 
-      return;
-    }
+  // const changePasswordHandler = (e) => {
+  //   setPassword(e.target.value);
+  // };
+  // const loginHandler = () => {
+  //   if (emailRef.current.value.length === 0) {
+  //     emailRef.current.focus();
+  //     emailRef.current.style.borderColor = "red";
+  //     emailRef.current.style.outline = "none";
+  //     setError("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
 
-    if (password.length === 0) {
-      passwordRef.current.focus();
-      passwordRef.current.style.borderColor = "red";
-      passwordRef.current.style.outline = "none";
-      setError(
-        "Credenciales incorrectas. Por favor, inténta rellendando con una contraseña "
-      );
-      return;
-    } else if (password.length <= 8) {
-      passwordRef.current.focus();
-      passwordRef.current.style.borderColor = "red";
-      passwordRef.current.style.outline = "none";
-      setError(
-        "Credenciales incorrectas. Por favor, inténtalo de nuevo con una contraseña más larga."
-      );
-      return;
-    }
-    alert(`Su email es: ${email} y su password es: ${password}`);
-  };
+  //     return;
+  //   }
+
+  //   if (password.length === 0) {
+  //     passwordRef.current.focus();
+  //     passwordRef.current.style.borderColor = "red";
+  //     passwordRef.current.style.outline = "none";
+  //     setError(
+  //       "Credenciales incorrectas. Por favor, inténta rellendando con una contraseña "
+  //     );
+  //     return;
+  //   } else if (password.length <= 8) {
+  //     passwordRef.current.focus();
+  //     passwordRef.current.style.borderColor = "red";
+  //     passwordRef.current.style.outline = "none";
+  //     setError(
+  //       "Credenciales incorrectas. Por favor, inténtalo de nuevo con una contraseña más larga."
+  //     );
+  //     return;
+  //   }
+  //   alert(`Su email es: ${email} y su password es: ${password}`);
+  // };
+
   return (
-    <>
-      <div className={`${theme === "DARK" && "dark-theme"}`}>
-        <NavBar />
-        <div className="container-fluid ml-0">
-          <div className="row ml-0">
-            <div className="col-md-6 d-flex align-items-center justify-content-center ml-0 ">
-              <img
-                src="../assets/mates-login.jpg" // Reemplaza con la ruta de tu imagen
-                alt="Imagen de inicio de sesión"
-                className="img-custom-size"
-              />
-            </div>
-            <div className="col-md-6 d-flex align-items-center p-5 ">
-              <div className="container">
-                <form className="border rounded-3 p-5 ">
-                  <h2>Iniciar sesión</h2>
-                  <div className="input-conteiner mt-3 mw-100 mb-4">
-                    
-                    <input
-                      className={`${theme === "DARK" ? "form-control form-control-lg bg-dark text-light":"form-control form-control-lg bg-light text-dark"}`}
-                      value={email}
-                      onChange={changeEmailHandler}
-                      placeholder="Ingrese su email"
-                      type="email"
-                      ref={emailRef}
-                    />
-                  </div>
+    <div className={`${theme === "DARK" && "dark-theme"}`}>
+      <NavBar />
+      <div className="container-fluid ml-0">
+        <div className="row ml-0">
+          <div className="col-md-6 d-flex align-items-center justify-content-center ml-0 ">
+            <img
+              src="../assets/mates-login.jpg" // Reemplaza con la ruta de tu imagen
+              alt="Imagen de inicio de sesión"
+              className="img-custom-size"
+            />
+          </div>
+          <div className="col-md-6 d-flex align-items-center p-5 ">
+            <div className="container">
+              <form className="border rounded-3 p-5 ">
+                <h2>Iniciar sesión</h2>
+                <div className="input-conteiner mt-3 mw-100 mb-4">
+                  <input
+                    className={`${
+                      theme === "DARK"
+                        ? "form-control form-control-lg bg-dark text-light"
+                        : "form-control form-control-lg bg-light text-dark"
+                    }`}
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Ingrese su email"
+                  />
+                </div>
 
-                  <div className="input-conteiner mt-3 mb-4">
-                    <input
-                      ref={passwordRef}
-                      value={password}
-                      onChange={changePasswordHandler}
-                      className={`${theme === "DARK" ? "form-control form-control-lg bg-dark text-light":"form-control form-control-lg bg-light text-dark "}`}
-                      placeholder="Ingrese su contraseña"
-                      type="password"
-                    />
-                  </div>
-                  <div className="text-danger">{error}</div>
-                  <div>
-                    <a href="#" className="link-primary">
-                      Deseas registrarte?Clickea aqui
-                    </a>
-                  </div>
-                  <div className="vstack mt-3 align-self-center">
-                    <button
-                      onClick={onLoginHandler}
-                      type="button"
-                      className="btn btn-outline-secondary "
-                    >
-                      Iniciar Sesion
-                    </button>
-                  </div>
-                </form>
-              </div>
+                <div className="input-conteiner mt-3 mb-4">
+                  <input
+                    className={`${
+                      theme === "DARK"
+                        ? "form-control form-control-lg bg-dark text-light"
+                        : "form-control form-control-lg bg-light text-dark"
+                    }`}
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Ingrese su contraseña"
+                  />
+                </div>
+                {/* <div className="text-danger">{error}</div> */}
+                <div>
+                  <a href="#" class="link-primary">
+                    Deseas registrarte? Clickea aqui
+                  </a>
+                </div>
+                <div className="vstack mt-3 align-self-center">
+                  <button
+                    onClick={handleSumbit}
+                    type="button"
+                    className="btn btn-outline-secondary "
+                  >
+                    Iniciar Sesion
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
