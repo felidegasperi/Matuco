338af9387da3459f3ebabc172b43eceabceffb14
felidegasperi <felidegasperi@gmail.com>
2023-11-24 21:06:21 -0300

    added edit status in listOrders (not fuctional)

--- matuco/src/components/listOrders/ListOrders.js
+++ matuco/src/components/listOrders/ListOrders.js
@@ -1,30 +1,67 @@
 import React, { useContext } from "react";
+
 import "./ListOrders.css";
+
 import { AuthenticationContext } from "../../services/authenticationContext/Authentication.context";
-import { useFetchOrders } from "../../hooks/useFetchOrders";
+// import { useFetchOrders } from "../../hooks/useFetchOrders";
 import { useNavigate } from "react-router-dom";
 import { APIContext } from "../../services/apiContext/API.context";
 import Loaders from "../ui/loaders/Loaders";
 
-const ListOrders = () => {
-  const { theme } = useContext(AuthenticationContext);
-  const { isLoading } = useContext(APIContext);
+const ListOrders = ({ orders }) => {
   const navigate = useNavigate();
 
-  const apiUrl = "https://matuco-fake-api.onrender.com/orders";
-  const { orders, error } = useFetchOrders(apiUrl);
+  const { theme } = useContext(AuthenticationContext);
+  const { isLoading } = useContext(APIContext);
 
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
+  // const apiUrl = "https://matuco-fake-api.onrender.com/orders";
+  // const { orders, error } = useFetchOrders(apiUrl);
+
+  // if (error) {
+  //   return <p>Error: {error.message}</p>;
+  // }
 
   const navigateProductsHandler = () => {
     navigate("/products");
   };
 
+  const changeStatusHandler = () => {
+    const confirmDelete = window.confirm(
+      "¿Estás seguro de que deseas cambiar el estado del pedido?"
+    );
+
+    //todavia esta condicion no hace nada, no apretar
+    // if (confirmDelete) {
+    //   //realiza el cambio de estado, de no entregado a entregado y viseversa
+    //   fetch(`https://matuco-fake-api.onrender.com/orders/${id}`, {
+    //     method: "PATCH",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     //falta condicional para ponerlo en true si esta en false y viseversa
+    //     body: JSON.stringify({ status: true }),
+    //   })
+    //     .then((response) => {
+    //       if (!response.ok) {
+    //         throw new Error("Error al eliminar el usuario");
+    //       }
+    //        setUsers((PrevUsers) =>
+    //          PrevUsers.map((user) =>
+    //            user.id === id ? { ...user, isActive: false } : user
+    //          )
+    //        );
+    //       alert("Se ha elimiado el usuario con exito!");
+    //     })
+    //     .catch((err) => {
+    //       console.Error("Error", err);
+    //       console.log("Error al eliminar el usuario");
+    //     });
+    // }
+  };
+
   return (
     <div className={`${theme === "DARK" && "dark-theme"}`}>
-      {orders.length === 0 ? (
+      {orders === false ? (
         <div className="d-flex flex-column align-items-center justify-content-center min-vh-100">
           <h2> No hay compras realizadas, por favor vuelva a Productos</h2>
           <div className="d-flex align-items-center justify-content-center">
@@ -56,7 +93,7 @@ const ListOrders = () => {
                 <th>Email comprador</th>
                 <th>Productos</th>
                 <th>Precio total</th>
-                <th>Opciones</th>
+                <th>Estado</th>
               </tr>
             </thead>
             {isLoading && <Loaders />}
@@ -75,9 +112,13 @@ const ListOrders = () => {
                       ))}
                     </ul>
                   </td>
-                  <td>{order.totalPrice}</td>
+                  <td>${order.totalPrice}</td>
                   <td>
                     {order.status === false ? "No entregado" : "Entregado"}
+                    <button onClick={() => changeStatusHandler()}>
+                      Cambiar estado a{" "}
+                      {order.status === false ? "Entregado" : "No entregado"}
+                    </button>
                   </td>
                 </tr>
               ))}
--- matuco/src/components/listOrders/OrdersContainer.js
+++ matuco/src/components/listOrders/OrdersContainer.js
@@ -3,15 +3,24 @@ import { AuthenticationContext } from "../../services/authenticationContext/Auth
 import NavBar from "../navBar/NavBar";
 import Footer from "../footer/Footer";
 import ListOrders from "./ListOrders";
+import { useFetchOrders } from "../../hooks/useFetchOrders";
 
 const OrdersContainer = () => {
   const { theme } = useContext(AuthenticationContext);
+
+  const apiUrl = "https://matuco-fake-api.onrender.com/orders";
+  const { orders, error } = useFetchOrders(apiUrl);
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
     <div className={`${theme === "DARK" && "dark-theme"}`}>
       <NavBar />
       <div className={`${theme === "DARK" && "dark"}`}>
         <div>
-          <ListOrders />
+          <ListOrders orders={orders} />
         </div>
       </div>
       <Footer />
