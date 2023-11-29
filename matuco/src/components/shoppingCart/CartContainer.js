c3f08fb2a68b8ef50167613c7f6d744ee27fe833
Alvaro Johansen <109972764+Alvarojohansen@users.noreply.github.com>
2023-11-29 01:37:16 -0300

    Revert "changes in cart Container"
    This reverts commit 6ebd1c9177d7b77af68762fa4d9bdff8c062ed13.

--- matuco/src/components/shoppingCart/CartContainer.js
+++ matuco/src/components/shoppingCart/CartContainer.js
@@ -68,21 +68,18 @@ const CartContainer = () => {
         })
         .then((newOrderData) => {
           setOrders([...orders, newOrderData]);
-          navigate("/products");
           localStorage.removeItem("cart");
-          alert("Compra finalizda. Gracias!");
-          // toast.success("Compra finalizada. Gracias!", {
-          //   position: "top-center",
-          //   autoClose: 5000,
-          //   hideProgressBar: false,
-          //   closeOnClick: true,
-          //   pauseOnHover: true,
-          //   draggable: true,
-          //   progress: undefined,
-          //   theme: "dark",
-          // });
         });
-
+      toast.success("Compra finalizada. Gracias!", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
       setCart([]);
     }
   };
